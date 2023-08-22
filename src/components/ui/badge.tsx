a4846849a807d3ebba7b2f54d16b4a2ebf1b9fd6
Michael Long <michaeltlong10@gmail.com>
2023-08-22 00:43:38 -0400

    added prisma generate to build command

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,9 +15,9 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
-        ['To-do']: "border-transparent bg-red-50",
-        ['In progress']: "border-transparent bg-yellow-50",
-        Done: "border-transparent bg-green-50",
+        ['To-do']: "text-stone-700 border-transparent bg-red-50",
+        ['In progress']: "text-stone-700 border-transparent bg-yellow-50",
+        Done: "text-stone-700 border-transparent bg-green-50",
       },
     },
     defaultVariants: {
