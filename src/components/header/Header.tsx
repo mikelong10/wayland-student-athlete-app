6c05fd3c8de5a8b45155b6d466dc6954b4feff67
Michael Long <michaeltlong10@gmail.com>
2023-08-22 15:22:13 -0400

    finalized nav styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,5 @@
-import { Button, buttonVariants } from "@components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@components/ui/sheet";
-import { Separator } from "@components/ui/separator";
-import { Menu } from "lucide-react";
 import Link from "next/link";
-import UserAccountNav from "./UserAccountNav";
 import { getCurrentUser } from "@lib/session";
-import InteractiveButton from "@components/ui/InteractiveButton";
-import { VariantProps } from "class-variance-authority";
 import { ActiveUser, HeaderNavLink } from "@lib/types";
 import MobileNav from "./MobileNav";
 import DesktopNav from "./DesktopNav";
@@ -21,28 +14,28 @@ const headerNavLinks: HeaderNavLink[] = [
     url: "/what",
     text: "What we do",
     variant: "ghost",
-    mobileStyle: "text-4xl tracking-tight",
+    mobileStyle: "text-4xl tracking-tight bg-transparent hover:bg-transparent underline-offset-8 hover:underline",
     desktopStyle: "h-10 w-fit items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground focus:bg-muted focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-muted/50 data-[state=open]:bg-muted/50",
   },
   {
     url: "/who",
     text: "Who we are",
     variant: "ghost",
-    mobileStyle: "text-4xl tracking-tight",
+    mobileStyle: "text-4xl tracking-tight bg-transparent hover:bg-transparent underline-offset-8 hover:underline",
     desktopStyle: "h-10 w-fit items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground focus:bg-muted focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-muted/50 data-[state=open]:bg-muted/50",
   },
   {
     url: "/reviews",
     text: "Reviews",
     variant: "ghost",
-    mobileStyle: "text-4xl tracking-tight",
+    mobileStyle: "text-4xl tracking-tight bg-transparent hover:bg-transparent underline-offset-8 hover:underline",
     desktopStyle: "h-10 w-fit items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground focus:bg-muted focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-muted/50 data-[state=open]:bg-muted/50",
   },
   {
     url: "/request",
     text: "Request a job",
     variant: "ghost",
-    mobileStyle: "rounded-full p-8 text-2xl tracking-tight",
+    mobileStyle: "text-2xl tracking-tight text-primary-foreground bg-primary hover:bg-primary/80 p-8 rounded-full",
     desktopStyle: "h-10 w-fit items-center justify-center rounded-md px-4 py-2 text-sm font-medium underline underline-offset-8 hover:no-underline transition-all hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-primary/50 data-[state=open]:bg-primary/50",
   },
 ];
