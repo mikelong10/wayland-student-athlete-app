25100b10d6fd6bff1ae654fa662ed9afdf0077a4
Michael Long <michaeltlong10@gmail.com>
2023-08-22 20:30:20 -0400

    added loading UI

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader2 } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen h-full w-full flex flex-col justify-center items-center">
+      <Loader2 className="h-10 w-10 animate-spin" />
+    </div>
+  );
+}
