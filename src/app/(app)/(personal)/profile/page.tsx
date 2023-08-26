04a3d5e67e901fb2a0a6ddb4ab309afd5be1de22
Michael Long <michaeltlong10@gmail.com>
2023-08-26 15:58:26 -0400

    removed button on profile page

--- src/app/(app)/(personal)/profile/page.tsx
+++ src/app/(app)/(personal)/profile/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@components/ui/button";
 
 export const metadata = {
   title: "Profile",
@@ -9,7 +8,6 @@ export default async function ProfilePage() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-extrabold tracking-tight">Profile</h1>
-      <Button type="button">Click me!</Button>
     </main>
   );
 }
