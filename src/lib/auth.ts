cdd72133c212cc96f59faf88b693373d626dad45
Michael Long <michaeltlong10@gmail.com>
2023-12-12 01:26:54 -0500

    added email account linking

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 
 import { db } from "./db";
 
@@ -18,85 +19,23 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID ?? "",
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true
     }),
-    //   EmailProvider({
-    //     from: process.env.SMTP_FROM,
-    //     sendVerificationRequest: async ({ identifier, url, provider }) => {
-    //       const user = await db.user.findUnique({
-    //         where: {
-    //           email: identifier,
-    //         },
-    //         select: {
-    //           emailVerified: true,
-    //         },
-    //       })
-
-    //       const templateId = user?.emailVerified
-    //         ? process.env.POSTMARK_SIGN_IN_TEMPLATE
-    //         : process.env.POSTMARK_ACTIVATION_TEMPLATE
-    //       if (!templateId) {
-    //         throw new Error("Missing template id")
-    //       }
-
-    //       const result = await postmarkClient.sendEmailWithTemplate({
-    //         TemplateId: parseInt(templateId),
-    //         To: identifier,
-    //         From: provider.from as string,
-    //         TemplateModel: {
-    //           action_url: url,
-    //           product_name: siteConfig.name,
-    //         },
-    //         Headers: [
-    //           {
-    //             // Set this to prevent Gmail from threading emails.
-    //             // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
-    //             Name: "X-Entity-Ref-ID",
-    //             Value: new Date().getTime() + "",
-    //           },
-    //         ],
-    //       })
-
-    //       if (result.ErrorCode) {
-    //         throw new Error(result.Message)
-    //       }
+    // EmailProvider({
+    //   server: {
+    //     host: process.env.EMAIL_SERVER_HOST,
+    //     port: process.env.EMAIL_SERVER_PORT,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER,
+    //       pass: process.env.EMAIL_SERVER_PASSWORD,
     //     },
-    //   }),
+    //   },
+    //   from: process.env.EMAIL_FROM,
+    // }),
   ],
-  // callbacks: {
-  //   async session({ token, session }) {
-  //     if (token) {
-  //       session.user.id = token.id
-  //       session.user.name = token.name
-  //       session.user.email = token.email
-  //       session.user.image = token.picture
-  //     }
-
-  //     return session
-  //   },
-  //   async jwt({ token, user }) {
-  //     const dbUser = await db.user.findFirst({
-  //       where: {
-  //         email: token.email,
-  //       },
-  //     })
-
-  //     if (!dbUser) {
-  //       if (user) {
-  //         token.id = user?.id
-  //       }
-  //       return token
-  //     }
-
-  //     return {
-  //       id: dbUser.id,
-  //       name: dbUser.name,
-  //       email: dbUser.email,
-  //       picture: dbUser.image,
-  //     }
-  //   },
-  // },
 };
