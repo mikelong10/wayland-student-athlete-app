53896111c04f72b69515349274b77d7afc3a01c1
Michael Long <michaeltlong10@gmail.com>
2023-12-19 15:11:51 -0500

    improved loading ui

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -5,9 +5,11 @@ import Header from "@components/header/Header";
 
 export default function Loading() {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center">
+    <div className="flex min-h-screen w-full flex-col items-center">
       <Header />
-      <Loader2 className="h-10 w-10 animate-spin" />
+      <div className="bg-background flex h-full min-h-screen w-full flex-col items-center justify-center">
+        <Loader2 className="h-10 w-10 animate-spin" />
+      </div>
       <Footer />
     </div>
   );
