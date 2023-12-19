5451a95b64b523e58baf03fbb4419997994fee32
Michael Long <michaeltlong10@gmail.com>
2023-12-19 15:18:24 -0500

    fixed loading ui duplicate header + footer

--- src/app/(app)/(personal)/loading.tsx
+++ src/app/(app)/(personal)/loading.tsx
@@ -1,16 +1,9 @@
 import { Loader2 } from "lucide-react";
 
-import { Footer } from "@components/footer/Footer";
-import Header from "@components/header/Header";
-
 export default function Loading() {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center">
-      <Header />
-      <div className="bg-background flex h-full min-h-screen w-full flex-col items-center justify-center">
-        <Loader2 className="h-10 w-10 animate-spin" />
-      </div>
-      <Footer />
+    <div className="bg-background flex h-full min-h-screen w-full flex-col items-center justify-center">
+      <Loader2 className="h-10 w-10 animate-spin" />
     </div>
   );
 }
