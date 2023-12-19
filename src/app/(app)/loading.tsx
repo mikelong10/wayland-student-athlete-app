64647d4a48bad4a9222dcc9136407ad9f1322e50
Michael Long <michaeltlong10@gmail.com>
2023-12-19 14:11:22 -0500

    added header and footer to loading ui

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -1,9 +1,14 @@
 import { Loader2 } from "lucide-react";
 
+import { Footer } from "@components/footer/Footer";
+import Header from "@components/header/Header";
+
 export default function Loading() {
   return (
-    <div className="bg-background flex h-full min-h-screen w-full flex-col items-center justify-center">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center">
+      <Header />
       <Loader2 className="h-10 w-10 animate-spin" />
+      <Footer />
     </div>
   );
 }
