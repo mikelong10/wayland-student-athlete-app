2bd9bb2b59e4b46f1ecceac26fdbb2302de0bee1
Michael Long <michaeltlong10@gmail.com>
2024-01-15 22:13:16 -0500

    fix google spreadsheet private key

--- src/app/api/jobs/[jobId]/spreadsheet/route.ts
+++ src/app/api/jobs/[jobId]/spreadsheet/route.ts
@@ -23,7 +23,7 @@ export async function POST(
     // initialize auth
     const serviceAccountAuth = new JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      key: process.env.GOOGLE_PRIVATE_KEY,
+      key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     });
 
--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
     // initialize auth
     const serviceAccountAuth = new JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      key: process.env.GOOGLE_PRIVATE_KEY,
+      key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     });
 
