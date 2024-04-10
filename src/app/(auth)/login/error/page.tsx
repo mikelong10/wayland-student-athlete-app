a87d930c64f8e1f5a7924d0a5a28ba9cd38707df
Michael Long <michaeltlong10@gmail.com>
2024-04-10 01:53:38 -0400

    fix: switch email signin to use "nodemailer"

---
+++ src/app/(auth)/login/error/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect } from "react";
+import { KeyRound } from "lucide-react";
+
+import H1 from "@components/typography/h1";
+import { Alert, AlertDescription, AlertTitle } from "@components/ui/alert";
+import { Button } from "@components/ui/button";
+
+export default function Error({ error }: { error: Error }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center justify-center gap-8 text-center">
+      <H1>Something went wrong!</H1>
+      <Alert className="text-accent-foreground max-w-prose text-left">
+        <KeyRound className="size-4" />
+        <AlertTitle>{"Sorry we couldn't log you in."}</AlertTitle>
+        <AlertDescription>
+          {
+            "Please go back and try again! If the problem persists, please contact us at waylandstudentathlete@gmail.com."
+          }
+        </AlertDescription>
+      </Alert>
+      <Link href={"/login"}>
+        <Button>Back to login</Button>
+      </Link>
+    </main>
+  );
+}
